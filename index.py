8abc7eac37f5fc3627876df194c5b937ffce1195
Sebastian Ferreira <io@fersebas.com>
2025-10-30 13:43:59 -0500

    This is now a broken calculator

--- index.py
+++ index.py
@@ -1,2 +1,9 @@
-# Write you code here
-username = "Tyler"
+
+# username = input("Whats your name?")
+
+# print("Hello, " + username)
+
+number1 = input("What is your first number?")
+number2 = input("What is your second number?")
+
+print(number1 + number2)
